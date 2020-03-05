4e22fc4b233329ad86edc44a854c803af6d8bfe3
Maharshi Patel <maharshi365@gmail.com>
2020-03-04 23:59:40 -0500

    2020-03-04 Solutions

--- Solutions/Graphs/keys_and_rooms.py
+++ Solutions/Graphs/keys_and_rooms.py
@@ -6,14 +6,14 @@ class Solution(object):
         """
 
         keys = rooms[0]
-        left_to_visit = list(range(1, len(rooms)))
+        left_to_visit = {i: True for i in range(1, len(rooms))}
 
         while(len(keys) > 0):
             curr_room = keys[0]
             if curr_room in left_to_visit:
-                keys = keys + rooms[curr_room]
+                keys += rooms[curr_room]
                 keys.pop(0)
-                left_to_visit.remove(curr_room)
+                del left_to_visit[curr_room]
             else:
                 keys.pop(0)
         if (len(left_to_visit) == 0):
